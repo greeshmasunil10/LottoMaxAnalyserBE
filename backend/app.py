85f74357d7559ee581733d8ae3f8bd9c8c33b0ca
greeshmasunil10 <greeshmasunil10@gmail.com>
2023-02-19 10:37:31 -0500

    changed main

--- backend/app.py
+++ backend/app.py
@@ -31,4 +31,5 @@ def download():
     return send_file(data_file, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
